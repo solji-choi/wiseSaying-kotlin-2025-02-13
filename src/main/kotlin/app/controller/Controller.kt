c3de78d752e3bc0d9c349d314de4115f4f05bbea
solji <solji.choi89@gmail.com>
2025-02-14 01:49:54 +0900

    3, 4

--- src/main/kotlin/app/controller/Controller.kt
+++ src/main/kotlin/app/controller/Controller.kt
@@ -2,11 +2,18 @@ package com.ll.app.controller
 
 import java.util.*
 
-class Controller {
+class Controller(
+    var lastId: Int = 0
+) {
+
     fun actionAdd(scanner: Scanner) {
         print("명언 : ")
         val content = scanner.nextLine()
         print("작가 : ")
         val author = scanner.nextLine()
+
+        lastId++
+
+        println("${lastId}번 명언이 등록되었습니다.")
     }
 }
