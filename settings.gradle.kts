fb0cd62bd3bc40062ce1670c48bf6ec604e7c84c
solji <solji.choi89@gmail.com>
2025-02-13 23:59:39 +0900

    setting

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "wiseSaying-kotlin-2025-02-13"
+
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+package com.ll
+
+fun main() {
+    val name = "Kotlin"
+
+    println("Hello, " + name + "!")
+
+    for (i in 1..5) {
+        println("i = $i")
+    }
+}
